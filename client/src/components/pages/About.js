e223367b17fd4554fbde4f0aa00764bfb731536e
AndrewMorrow <beardedmongo@gmail.com>
2021-01-24 22:33:50 -0600

    Full CRUD on contacts complete and filter implemplemented

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About This App</h1>
+            <p classname="my-1">
+                This is a full-stack React app for keeping contacts.
+            </p>
+            <div className="bg-dark p">
+                <strong>Version: 1.0.0</strong>
+            </div>
+        </div>
+    );
+};
+
+export default About;
