6a3934fab3ab5e45849dac6140e4f5eb35eeb022
AndrewMorrow <beardedmongo@gmail.com>
2021-01-25 21:08:50 -0600

    production ready

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -13,6 +14,16 @@ connectDB();
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Server static assets in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    );
+}
 
 app.listen(PORT, () =>
     console.log(`Server listening at http://localhost:${PORT}`)
