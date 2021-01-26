25d54653e6dcc5e02647ff29667886a29e648088
AndrewMorrow <beardedmongo@gmail.com>
2021-01-25 20:34:11 -0600

    Full CRUD server implemented

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
-import { FaIdCardAlt } from "react-icons/fa";
+import { FaIdCardAlt, FaSignOutAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import AuthContext from "../../context/auth/authContext";
+import ContactContext from "../../context/contact/contactContext";
 
 const Navbar = ({ title }) => {
+    const authContext = useContext(AuthContext);
+    const contactContext = useContext(ContactContext);
+    const { isAuthenticated, logout, user } = authContext;
+    const { clearContacts } = contactContext;
+
+    const onLogout = () => {
+        logout();
+        clearContacts();
+    };
+
+    const authLinks = (
+        <>
+            <li> Welcome back {user && user.name}</li>
+            <li>
+                <a href="#!" onClick={onLogout}>
+                    <FaSignOutAlt /> <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </>
+    );
+
+    const guestLinks = (
+        <>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/register">Register</Link>
+            </li>
+            <li>
+                <Link to="/login">Login</Link>
+            </li>
+        </>
+    );
     return (
         <div className="navbar bg-primary">
             <h1>
                 <FaIdCardAlt /> {title}
             </h1>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
+            <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
         </div>
     );
 };
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,8 +1,10 @@
 import React, { useReducer } from "react";
-import { v4 as uuidv4 } from "uuid";
 import ContactContext from "./contactContext";
 import contactReducer from "./contactReducer";
+import axios from "axios";
 import {
+    GET_CONTACTS,
+    CLEAR_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
@@ -10,48 +12,80 @@ import {
     UPDATE_CONTACT,
     FILTER_CONTACTS,
     CLEAR_FILTER,
+    CONTACT_ERROR,
 } from "../types";
 
 const ContactState = (props) => {
     const initialState = {
-        contacts: [
-            {
-                type: "personal",
-                id: 1,
-                name: "Ted Johnson",
-                email: "tjohnson@gmail.com",
-                phone: "111-111-1111",
-            },
-            {
-                type: "professional",
-                id: 2,
-                name: "Sarah Smith",
-                email: "ssmith@gmail.com",
-                phone: "111-111-1111",
-            },
-            {
-                type: "professional",
-                id: 3,
-                name: "Don Smith",
-                email: "dsmith@gmail.com",
-                phone: "111-111-1111",
-            },
-        ],
+        contacts: null,
         current: null,
         filtered: null,
+        error: null,
     };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
 
+    // Get Contacts
+    const getContacts = async () => {
+        try {
+            const res = await axios.get("/api/contacts");
+            dispatch({ type: GET_CONTACTS, payload: res.data });
+        } catch (error) {
+            dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+        }
+    };
+
     // Add Contact
-    const addContact = (contact) => {
-        contact.id = uuidv4();
-        dispatch({ type: ADD_CONTACT, payload: contact });
+    const addContact = async (contact) => {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        try {
+            const res = await axios.post("/api/contacts", contact, config);
+            dispatch({ type: ADD_CONTACT, payload: res.data });
+        } catch (error) {
+            dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+        }
     };
 
     // Delete Contact
-    const deleteContact = (id) => {
-        dispatch({ type: DELETE_CONTACT, payload: id });
+    const deleteContact = async (id) => {
+        try {
+            await axios.delete(`/api/contacts/${id}`);
+
+            dispatch({ type: DELETE_CONTACT, payload: id });
+        } catch (error) {
+            dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+        }
+    };
+
+    // Update Contact
+    const updateContact = async (contact) => {
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        try {
+            const res = await axios.put(
+                `/api/contacts/${contact._id}`,
+                contact,
+                config
+            );
+
+            dispatch({ type: UPDATE_CONTACT, payload: res.data });
+        } catch (error) {
+            dispatch({ type: CONTACT_ERROR, payload: error.response.msg });
+        }
+    };
+
+    // Clear Contacts
+    const clearContacts = (id) => {
+        dispatch({ type: CLEAR_CONTACTS });
     };
 
     // Set Current Contact
@@ -62,11 +96,6 @@ const ContactState = (props) => {
     // Clear Current Contact
     const clearCurrent = () => {
         dispatch({ type: CLEAR_CURRENT });
-    };
-
-    // Update Contact
-    const updateContact = (contact) => {
-        dispatch({ type: UPDATE_CONTACT, payload: contact });
     };
 
     // Filter Contacts
@@ -92,6 +121,9 @@ const ContactState = (props) => {
                 updateContact,
                 filterContacts,
                 clearFilter,
+                getContacts,
+                clearContacts,
+                error: state.error,
             }}
         >
             {props.children}
